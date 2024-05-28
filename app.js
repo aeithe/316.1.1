ffb1a59446c17458a0b98e4090cd20cc67d16a30
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-28 16:48:23 -0400

    created a function for part 3

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
 const mainEl = document.querySelector("main");
 mainEl.style.background = "var(--main-bg)";
 const headOne = document.createElement("h1");
-headOne.innerText = "hey what's up?";
+headOne.innerText = "hey, what's up?";
 mainEl.append(headOne);
 mainEl.classList.add("flex-ctr");
+
+
 
 const topMenueEl = document.getElementById("top-menu");
 topMenueEl.style.height = "100%";
 topMenueEl.style.background = "var(--top-menu-bg)"
 topMenueEl.classList.add("flex-around");
+
+//const aboutNavigation = document.createElement("a");
+//console.log(menuLinks.length);
+function menuLinksFunction(menuLinks){
+ for(let i = 0; i> menuLinks.length; i++){
+    if(menuLinks[i].type ==="href"){
+        return document.createElement("menuLinks[i]");
+    }
+ }
+};
+console.log(menuLinksFunction(menuLinks));
