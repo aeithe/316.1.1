b70bd32bf495332b92747a21d404622836926206
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-29 10:47:47 -0400

    fixed last part of this with help of youtube video from steven

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var menuLinks = [
+const menuLinks = [
     { text: 'about', href: '/about' },
     { text: 'catalog', href: '/catalog' },
     { text: 'orders', href: '/orders' },
@@ -14,18 +14,25 @@ mainEl.classList.add("flex-ctr");
 
 
 
-const topMenueEl = document.getElementById("top-menu");
-topMenueEl.style.height = "100%";
-topMenueEl.style.background = "var(--top-menu-bg)"
-topMenueEl.classList.add("flex-around");
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.background = "var(--top-menu-bg)"
+topMenuEl.classList.add("flex-around");
 
 //const aboutNavigation = document.createElement("a");
 //console.log(menuLinks.length);
-function menuLinksFunction(menuLinks){
- for(let i = 0; i> menuLinks.length; i++){
-    if(menuLinks[i].type ==="href"){
-        return document.createElement("menuLinks[i]");
-    }
- }
-};
-console.log(menuLinksFunction(menuLinks));
+// function menuLinksFunction(menuLinks){
+//  for(let i = 0; i> menuLinks.length; i++){
+//     if(menuLinks[i].type ==="href"){
+//         return document.createElement("menuLinks[i]");
+//     }
+//  }
+// };
+// console.log(menuLinksFunction(menuLinks));
+
+menuLinks.forEach((link) =>{
+  const aElement = document.createElement("a");
+  aElement.href = link.href;
+  aElement.textContent = link.text;
+  topMenuEl.appendChild(aElement)
+})
